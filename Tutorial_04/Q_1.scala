daab5127e881d96a6b684c9f95ac47319045799d
Nareash <nareash20010150@gmail.com>
2022-08-02 09:16:26 +0530

    Modified question number 1

--- Tutorial_04/Q_1.scala
+++ Tutorial_04/Q_1.scala
@@ -7,7 +7,8 @@ object Q_1 {
 
   def calculateInterest(amount: Double): Double = amount match {
     case x if(x < 20000) => x * 0.02
-    case x if(x < 200000) => x * 0.035
+    case x if(x < 200000) => x * 0.04
+    case x if(x < 2000000) => x * 0.035
     case x => x * 0.065
   }
 
